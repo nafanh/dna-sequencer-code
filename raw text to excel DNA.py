773fb901aed9e5bb0d77a30ba60a27413df7a84c
nafanh <topkekoutof10@gmail.com>
2019-07-01 16:05:40 -0500

    Update raw text to excel DNA.py

--- raw text to excel DNA.py
+++ raw text to excel DNA.py
@@ -2,8 +2,20 @@ import pandas as pd
 import numpy as np
 import re
 import pprint
+import matplotlib.pyplot as plt
+
+#Gets a number from string and sorts it into a list
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split(r'(\d+)',text)]
 
 
+
+#Entered name has to be in this format:
+# Any_Any_[Time]_Any(even amt of underscores)_[Well](.fsa)
+#ex: Exo_BurstMM_0.5_TLD_2018-12-18_A06.fsa
 def filtered_data(name):
     # file_name = 'Burst on PThio DNA.txt'
     f = open(name, 'r')
@@ -12,22 +24,26 @@ def filtered_data(name):
     f.readline()
     col_data = []
     count = 0
+    # reads each line in the text file
     for test in f:
+        #Splits column data based on header
+        # Dye/Sample Peak,Sample File Name,Size, Height, Area,Data Point
         test_set = test.split()
+        #print(test_set)
         # strips the underscore in long description
         desc_fix = test_set[1].split('_')
-        # print(desc_fix)
+        #print(desc_fix)
         # removes long description name
         test_set.pop(1)
-        # print(test_set)
-        # print(test_set)
+        #print(test_set)
+        #print(test_set)
         
         #inserts time into test_set
         test_set.insert(2, desc_fix[2])
 
         #inserts Well number at the end of test_set
         test_set.insert(3, desc_fix[len(desc_fix) - 1])
-        # print(test_set)
+        #print(test_set)
         
         # splits dye and peak number and puts them into separate columns
         dye_peak = test_set[0].strip('\"').replace(',', '')
@@ -37,27 +53,33 @@ def filtered_data(name):
             test_set.insert(i, dye_peak[i])
             i += 1
         # print(test_set)
+        # adding all data to each column
         col_data.append(test_set)
         count += 1
     # pprint.pprint(col_data)
     df = pd.DataFrame(col_data, columns=headers)
+    #print(col_data)
 
-    # filters height above 100
-    df_h100 = df.loc[df['Height'].astype(int) > 100]
+    # filters height above user input. Note that if filter height
+    # is above internal standard height, then error will raise
+    # have to add try/except block here for future use
+    min_height = int(input("Please enter the minimum height: "))
+    df_hmin = df.loc[df['Height'].astype(int) > min_height]
 
     # exports data with height above 100 to excel sheet
-    export_excel_filtered = df.to_csv('Export_data_filtered_H100.csv',sep=',')
+    #export_excel_filtered = df.to_csv('Export_data_filtered_Hmin.csv',sep=',')
     f.close()
-    return df_h100
+    return df_hmin
 
 
 def sample_distance(filtered_data):
     # gets peaks without internal standard
     df_no_int = filtered_data.loc[filtered_data['Dye'] == 'B']
-    # print(df_no_int)
+    #print(df_no_int)
 
     # gets peaks with internal standard
     df_int_std = filtered_data.loc[filtered_data['Dye'] == 'Y']
+   
     #Exports the internal standard data
     export_int_std = df_int_std.to_csv('Export_data_int_std.csv',sep = ',')
     # print(df_int_std)
@@ -78,6 +100,8 @@ def sample_distance(filtered_data):
     int_std_dict = dict(zip(int_stdtimelist, int_stdlist))
     sample_2d = [list(a) for a in zip(sample_timelist, sample_list)]
 
+    #pprint.pprint(sample_2d)
+    
     # if sample_2d[i][0] in int_std_dict:
     diff_list = []
     for i in range(len(sample_2d)):
@@ -85,20 +109,86 @@ def sample_distance(filtered_data):
         diff_list.append(diff)
 
     df_no_int['Diff'] = diff_list
-    return (df_no_int)
 
     # exports data with compared to internal standard
-    export_excel_difference = df.to_csv('Export_data_intstd_Diff.csv',sep=',')
+    #export_excel_difference = df.to_csv('Export_data_intstd_Diff.csv',sep=',')
+    
+    return (df_no_int)
+
+# Returns pandas series with list of unique ranges (distance from int. std.)
+def diff_list(df):
+    diff_set = df['Diff'].tolist()
+    diff_set = sorted(list(set(diff_set)))
+    return pd.Series(diff_set)
 
 
 # Function that outputs the polymer size (ex: 28mer)
 # and places it into a new column
+# ****Have to edit to where the template size is in relation to difference***
 def size(df):
     #Asks user input for template size
-    original = int(input('Please enter the template size: '))
+    #original = int(input('Please enter the template size: '))
     #Adjusts the peak number in relation to template size
-    df["Size"] = df["Peak Number"].astype(int) + (original - 1)
+    length = []
+    ranges_list = []
+    print("Please enter the diff bounds for each polymer. Ex: 27mer/300-400")
+    addit = 'y'
+    while addit == 'y' or addit == 'Y':
+        temp = []
+        polymer = input("Enter polymer (ex:27mer): ")
+        length.append(polymer)
+        try:
+
+
+        
+            low_r = int(input("Enter lower bound of diff: "))          
+            upper_r = int(input("Enter upper bound of diff: "))
+            temp.append(low_r)
+            temp.append(upper_r)
+            ranges_list.append(temp)
+            addit = input("Are there any more? Input 'y' for yes and 'n' to end: ")
+        except ValueError:
+            print("Not valid number please try again")
+    
+    #ranges_list2 = sorted(ranges_list, key=lambda x: int("".join([i for i in x if i.isdigit()])))
+    
+        
+    ranges_dict = {}
+    for i in range(len(ranges_list)):
+        ranges_dict[length[i]] = ranges_list[i]
+    #print(ranges_dict)
+
+    diff_list = df['Diff'].astype(int).tolist()
+    ranges_keys = list(ranges_dict.keys())
+    #print(ranges_keys)
+    final_length_list = []
+    #print(diff_list)
+    for j in diff_list:
+        for i in range(len(ranges_keys)):
+            width = ranges_dict[ranges_keys[i]]
+            #print(ranges_dict[ranges_keys[i]])
+            low = width[0]
+            high = width[-1]
+            if j >= low and j <= high:
+                final_length_list.append(ranges_keys[i])
+               
+    #print(final_length_list)
+            
+    df["Size"] = final_length_list
+
+
+    
     return df
+
+
+# Returns a set list for x labels (size)
+##def get_x_values(df):
+##    df1 = list(set(df['Size'].tolist()))
+##    print(df1)
+##    length_list = list(set(df["Size"].tolist()))
+##    length_list = sorted(length_list, key=lambda x: int("".join([i for i in x if i.isdigit()])))
+#return length_list
+
 
 # Function that creates the table containing area values for each polymer
 def table(df):
@@ -108,8 +198,8 @@ def table(df):
 
     #Creates sorted set with size values (unique values only)
     df_size = list(set(df['Size'].tolist()))
-    df_size.sort()
-    
+    df_size.sort(key=natural_keys)
+    #print(df_size)
     #n = pd.DataFrame(columns = df_size)
     # headings = df_size
     # headings.append('Total')
@@ -126,9 +216,10 @@ def table(df):
 
     #Creates dictionary where time and size are keys. Value is area
     size_list = df['Size'].tolist()
-
+    
     #Creates tuple of time,size pairs
     two_keys = list(zip(time_list,size_list))
+    #print(two_keys)
     
     new_dict = {}
 
@@ -136,6 +227,9 @@ def table(df):
     n = pd.DataFrame()
     n.index.name = 'Time'
 
+    for size in df_size:
+        n[size] = ""
+    #print(n)
     #Appends area to dictionary containing the time and size keys
     for i in range(len(two_keys)):
         new_dict[two_keys[i]] = area_list[i]
@@ -144,9 +238,10 @@ def table(df):
     #for area
     for key in two_keys:
         time = key[0]
-        size = str(key[1]) + 'mer'
+        size = key[1] 
         n.loc[time,size] = int(new_dict[key])
 
+    #print(new_dict)
     #Creates new columns containing the area of each polymer/total
     n['Total'] = n.sum(axis=1)
     headers_list = n.columns.values.tolist()
@@ -161,6 +256,26 @@ def table(df):
     #Exports non-concentration fixed data
     export_before_conc = n.to_csv('Export_data_Before_concfix.csv',sep=',')
     return n
+
+# Returns a set list for x labels (size)
+def get_size_values(df):
+    df1 = list(set(df['Size'].tolist()))
+    return df1
+##    length_list = list(set(df["Size"].tolist()))
+##    length_list = sorted(length_list, key=lambda x: int("".join([i for i in x if i.isdigit()])))
+#return length_list
+
+# Gets the values 
+def get_frac_values(df):
+    col_length = df.shape[1]
+    midpt = int((col_length)/2) + 1
+    df1 = df.iloc[:,midpt:]
+    df1_values = df1.values.tolist()
+    return df1_values
+#Gets the time values from the index
+def get_time_values(df):
+    df_time = df.index.tolist()
+    return df_time
 
 #Function that multiplies values by specific concentration
 def conc_fix(df):
@@ -178,28 +293,62 @@ def conc_fix(df):
     export = df.to_csv('Export_data_After_concfix.csv',sep=',')
     return df
 
-
+# Makes plots of the fractional area vs size
+def plot(time,size,frac):
+    f,axarr = plt.subplots(len(frac)//2 ,2,sharex=True,sharey=True)
+    f.suptitle('Fractional Area vs. Size')
+    count = 0
+    for i in range(2):
+        for j in range(len(time)//2):
+            axarr[j,i].bar(size,frac[count],width=0.1)
+            axarr[j,i].set_title('Time: ' + str(time[count]),loc='right',fontsize=8)
+            #print(count)
+            count+=1
+    f.text(0.04,0.5,'Fractional Area', va='center', rotation='vertical')
+    plt.subplots_adjust(hspace=0.4)
+    plt.xticks(np.arange(len(size)),size)
+    plt.xlabel('Size')
+    #plt.ylabel('Fractional Area',labelpad=20)
+    plt.show()
+    
+# Takes lits from get_frac_values and get_x_values
+##def make_graphs(frac,size,time)
 def main():
     name = input('Enter file name (.txt): ')
-    filtered = filtered_data(name)
+    filtered = filtered_data(name) #Creates table filtered by height threshold
+
+    #Adds column to table for distance to int. std.
     int_std_dist = sample_distance(filtered)
+
+    #Returns a pandas series of difference ranges
+    print(diff_list(int_std_dist))
+    print(int_std_dist)
     polymer = size(int_std_dist)
     print('Here is the Data (Filters out heights below threshold. Note no internal std):')
     print('--------------------------------------------------------')
     print(polymer)
+    #get_size_values(polymer)
     print()
     a = table(polymer)
     print('Here is the data (Before concentration fix):')
     print('--------------------------------------------------------')
     
     print(a)
-    print()
-    # print(int_std_dist)
-    fix = conc_fix(a)
-    print()
-    print('Here is the updated table data (after concentration fix):')
-    print('--------------------------------------------------------')
-    print(fix)
+    time = get_time_values(a)
+    length = get_size_values(polymer)
+    frac = get_frac_values(a)
+   
+    plot(time,length,frac)
+    
+##    print(get_frac_values(a))
+##    print(a.index.values.tolist())
+##    print()
+##    # print(int_std_dist)
+##    fix = conc_fix(a)
+##    print()
+##    print('Here is the updated table data (after concentration fix):')
+##    print('--------------------------------------------------------')
+##    print(fix)
 
 
 main()
